d0e1569643a43b85f8afe524fad78b023b1128eb
Lancelot de Ferrière <wraitii@gmail.com>
2024-05-23 16:35:29 +0200

    Sender -> Origin

--- hyle-contract/src/lib.rs
+++ hyle-contract/src/lib.rs
@@ -5,10 +5,12 @@ extern crate alloc;
 use serde::{Deserialize, Serialize};
 use alloc::{vec::Vec, string::String};
 
+// This is intended as the "typical" input a Hyle smart contract would receive.
+// Note that there is actually no requirement to match this structure.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct HyleInput<T> {
     pub initial_state: Vec<u8>,
-    pub sender: String,
+    pub origin: String,
     pub caller: String,
     pub block_number: u64,
     pub block_time: u64,
@@ -16,12 +18,15 @@ pub struct HyleInput<T> {
     pub program_inputs: T
 }
 
+// This struct should be used as the output (public witness) of a Hyle smart contract.
+// The protocol enforces constraints on the non-generic fields.
+// See the documentation for details.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct HyleOutput<T> {
     pub version: u32,
     pub initial_state: Vec<u8>,
     pub next_state: Vec<u8>,
-    pub sender: String,
+    pub origin: String,
     pub caller: String,
     pub block_number: u64,
     pub block_time: u64,
