0411f648f1759d7e6e30aa7027c16c45d76a0ae6
maximilien-hyle <maximilien@hyle.eu>
2024-05-23 16:32:10 +0200

    wip sp1 shrink

--- sp1-verifier/src/main.rs
+++ sp1-verifier/src/main.rs
@@ -1,8 +1,10 @@
-use std::env;
+use std::{env, fs::File, io::Read};
 
 use base64::prelude::*;
 
-use sp1_sdk::{ProverClient, SP1Proof, SP1VerifyingKey};
+use sp1_core::utils::BabyBearPoseidon2;
+use sp1_prover::SP1ReduceProof;
+use sp1_sdk::{InnerSC, SP1Prover, SP1Proof, SP1VerifyingKey};
 
 use hyle_contract::HyleOutput;
 
@@ -18,14 +20,27 @@ fn main() {
     let vk: SP1VerifyingKey = SP1VerifyingKey{
         vk: serde_json::from_str(&vk_json).unwrap()
     };
-    let mut proof = SP1Proof::load(&args[2]).expect("loading proof failed");
+    // let mut proof = SP1Proof::load(&args[2]).expect("loading proof failed");
+
+    // Load the shrinked proof.
+    let mut file = File::open(&args[2]).unwrap();
+    let mut bytes = Vec::new();
+    file.read_to_end(&mut bytes).unwrap();
+
+    let shrinked_proof: SP1ReduceProof<BabyBearPoseidon2> = bincode::deserialize::<SP1ReduceProof<InnerSC>>(&bytes).unwrap();
 
     // TODO: check that vk is correct ?
-    let prover_client = ProverClient::new();
-    prover_client.verify(&proof, &vk).expect("verification failed");
+    let prover = SP1Prover::new();
+    // prover.verify(&proof, &vk).expect("verification failed");
+    prover.verify_shrink(&shrinked_proof, &vk).expect("verification failed");
+
+    let public_values_slice = shrinked_proof.proof.public_values;
+    // pourquoi public values c'est un Vec<BabyBear> ... ?
+
+    println!("{:?}", output);
 
     // Outputs to stdout for the caller to read.
-    let output: HyleOutput<()> = proof.public_values.read::<HyleOutput<()>>();
-    println!("{}", serde_json::to_string(&output).expect("Failed to serialize output"));
+    // let output: HyleOutput<()> = proof.public_values.read::<HyleOutput<()>>();
+    // println!("{}", serde_json::to_string(&output).expect("Failed to serialize output"));
 
 }
