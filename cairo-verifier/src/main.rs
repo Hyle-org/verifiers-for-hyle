a31a7693794b02989ca14866796488750eb965b4
maximilien-hyle <maximilien@hyle.eu>
2024-06-21 17:19:48 +0200

    chore: make it wasm friendly

--- cairo-verifier/src/main.rs
+++ cairo-verifier/src/main.rs
@@ -1,30 +1,38 @@
 use clap::Parser;
 use stark_platinum_prover::proof::options::{ProofOptions, SecurityLevel};
-use cairo_verifier;
+use crate::utils::error::VerifierError;
 
 mod commands;
+mod utils;
 
 #[derive(Parser)]
 struct Cli {
     proof_path: String,
 }
 
-fn main(){
+fn main() -> Result<(), VerifierError> {
     let proof_options = ProofOptions::new_secure(SecurityLevel::Conjecturable100Bits, 3);
     let args: commands::ProverArgs = commands::ProverArgs::parse();
 
-    match args.entity {
+    let res = match args.entity {
         commands::ProverEntity::Verify(args) => {   
-            cairo_verifier::verify_proof(&args.proof_path, proof_options);
+            utils::verify_proof(&args.proof_path, proof_options)
         },
         commands::ProverEntity::Prove(args) => {
-            cairo_verifier::prove(
+            utils::prove(
                 &args.trace_bin_path,
                 &args.memory_bin_path,
                 &args.output_path,
                 &args.proof_path,
                 &proof_options
-            );
+            )
         }
-    }
+    };
+    match res {
+        Result::Ok(output) => println!("{}", output),
+        Result::Err(err) => {
+            return Err(err);
+        }
+    };
+    Ok(())
 }
